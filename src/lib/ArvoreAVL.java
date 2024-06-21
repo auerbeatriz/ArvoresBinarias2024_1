5f1a6ce7e16862fbb8e998b0705f9be45719211d
auerbeatriz <biaauer03@gmail.com>
2024-06-21 20:24:04 -0300

    feat: algoritmos de balanceamento da arvore avl

--- src/lib/ArvoreAVL.java
+++ src/lib/ArvoreAVL.java
@@ -7,7 +7,34 @@ public class ArvoreAVL<T> extends ArvoreBinaria<T> {
     public ArvoreAVL(Comparator<T> comparator) {
         super(comparator);
     }
-    
+
     //Implementar métodos para efetuar o balanceamento e sobrescrever método de adicionar elemento...
 
+    private No<T> rotacaoEsquerda(No<T> raizDesbalanceada) {
+        No<T> novaRaiz = raizDesbalanceada.getFilhoDireita();
+        raizDesbalanceada.setFilhoDireita(novaRaiz.getFilhoEsquerda());
+        novaRaiz.setFilhoEsquerda(raizDesbalanceada);
+
+        return novaRaiz;
+    }
+
+    private No<T> rotacaoDireita(No<T> raizDesbalanceada) {
+        No<T> novaRaiz = raizDesbalanceada.getFilhoEsquerda();
+        raizDesbalanceada.setFilhoEsquerda(novaRaiz.getFilhoDireita());
+        novaRaiz.setFilhoDireita(raizDesbalanceada);
+
+        return novaRaiz;
+    }
+
+    private No<T> rotacaoEsquerdaDireita(No<T> raizDesbalanceada) {
+        No<T> novaRaiz = raizDesbalanceada.getFilhoEsquerda();
+        raizDesbalanceada.setFilhoEsquerda(this.rotacaoEsquerda(novaRaiz));
+        return this.rotacaoDireita(raizDesbalanceada);
+    }
+
+    private No<T> rotacaoDireitaEsquerda(No<T> raizDesbalanceada) {
+        No<T> novaRaiz = raizDesbalanceada.getFilhoDireita();
+        raizDesbalanceada.setFilhoDireita(this.rotacaoDireita(novaRaiz));
+        return this.rotacaoEsquerda(raizDesbalanceada);
+    }
 }
